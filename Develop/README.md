728e32bba45056f22d62b2e3a1eb60845a2cc113
Lalu423 <>
2023-05-16 23:37:24 -0400

    added functions to create README file and prompt user

---
+++ Develop/README.md
@@ -0,0 +1,23 @@
+# j
+*[Description](#description)
+*[Installation](#installation)
+*[Usage](#usage)
+*[Contribution](#contribution)
+*[test](#test)
+*[License](#license)
+*[Questions](#questions)
+# Description
+    j
+# Installation
+    j
+## Usage
+    j
+## Contribution
+    j
+## Test
+    j
+## License
+    Other
+## Questions
+    j
+    j
--- Develop/index.js
+++ Develop/index.js
@@ -4,11 +4,91 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps to install this project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'When and how will this project be used?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide a list of collaborators, third-party assets and tutorials used.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'tbd',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Describe your project',
+        choices: ['The MIT License', 'Other', 'GPLv2', 'Apache'],
+    },
+    {
+        type: 'input',
+        name: 'git',
+        message: 'Enter GitHub username:',
+    }, {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your e-mail:',
+    },
+])
+    .then((answers) => {
+        const readMeContent = generateREADME(answers);
+
+        fs.writeFile('README.md', readMeContent, (err) =>
+            err ? console.log(err) : console.log('Congratulations on your new README!')
+        );
+    })
+
+const generateREADME = ({ title, description, installation, usage, contribution, test, license, git, email }) =>
+    `# ${title}
+    *[Description](#description)
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[Contribution](#contribution)
+    *[test](#test)
+    *[License](#license)
+    *[Questions](#questions)
+    # Description
+    ${description}
+    # Installation
+    ${installation}
+    ## Usage
+    ${usage}
+    ## Contribution
+    ${contribution}
+    ## Test
+    ${test}
+    ## License
+    ${license}
+    ## Questions
+    ${git}
+    ${email}`;
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() { }
 
 // Function call to initialize app
-init();
+//init();
